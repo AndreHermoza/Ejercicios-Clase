5cf92e6063e9fd333fe1debaa70b5323fbf97891
AndreHermoza <167846400+AndreHermoza@users.noreply.github.com>
2024-04-24 00:16:54 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,4 @@
+num1 = int(input("Ingrese su primer número: "))
+num2 = int(input("Ingrese su segundo número: "))
+suma = num1 + num2
+print("El resultado de la suma es: ", suma)
---
+++ ejercicio2.py
@@ -0,0 +1,3 @@
+nombre = str(input("Inserte su nombre: "))
+edad = int(input("Inserte su edad: "))
+print("Su nombre es ",nombre, "y su edad es", edad,"años")
