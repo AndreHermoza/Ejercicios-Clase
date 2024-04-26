7bf0a094768f7c30ee18aea4552978f13bca2c22
AndreHermoza <167846400+AndreHermoza@users.noreply.github.com>
2024-04-26 12:15:04 -0500

    Add files via upload

---
+++ articulos.py
@@ -0,0 +1,9 @@
+##ingresar datos
+cantA = int(input("Ingrese la cantidad de artpiculos a comprar: "))
+##comparación
+if(cantA < 8) :
+    print("La cantidad de articulos es: ", cantA)
+    print("Dirígase a la caja rápida")
+else : 
+    print("La cantidad de articulos es: ", cantA)
+    print("Dirígase a la a caja normal")
